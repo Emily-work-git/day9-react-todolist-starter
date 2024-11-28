2444a1d6b0705aee34a0602a79601d0216546a13
Emily Ng <ngem3@oocl.com>
2024-11-29 00:09:53 +0800

    feat: add/remove line through over text when clicked

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import { TodoContext } from '../App';
 export default function TodoItem(props){
+    const { dispatch } = React.useContext(TodoContext);
+
+    const handleClick = () => {
+        console.log(props.item)
+        console.log("clicked")
+        dispatch({type: "UPDATE_DONE", 
+            payload: {id: props.item.id}})
+    }
+
     return(
-        <div>
-        <span>{props.item.text}</span>
+        <div onClick={handleClick} style={{
+            textDecoration: props.item.done ? "line-through" : "none"
+        }}>
+            {props.item.text}
         </div>
     )
 }
--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -1,7 +1,17 @@
 export const todoReducer = (state, action) => {
-  switch(action.type){
+  switch (action.type) {
     case "ADD":
-      return [...state, {id: Date.now(), text: action.payload.text, done: false}];
+      return [
+        ...state,
+        { id: Date.now(), text: action.payload.text, done: false },
+      ];
+    case "UPDATE_DONE":
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, done: !todo.done };
+        }
+        return todo;
+      });
     default:
       return state;
   }
