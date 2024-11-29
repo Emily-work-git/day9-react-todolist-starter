87764b850f7dcb8115dca9be760cb07945b41528
Emily Ng <ngem3@oocl.com>
2024-11-29 11:20:56 +0800

    feat: add navigation bar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { createContext, useReducer } from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
 import { todoReducer } from "./context/todoReducer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import {Navigate} from "react-router-dom";
+import DoneList from "./components/DoneList";
 
 export const TodoContext = createContext();
 
@@ -13,15 +14,24 @@ function App() {
 
   return (
     <div className="App">
+
       <TodoContext.Provider value={{ state, dispatch }}>
-        <Router>
-           <Routes>
+      <Router>
+        <nav>
+          <Link to="/todo-list">Todo List </Link>
+          /
+          <Link to="/done-list">Done List </Link>
+        </nav>
+          <Routes>
               <Route path="*" element={<NotFound/>} />
               <Route path="/" element={<Navigate to="/todo-list"/>} />
+              <Route path="/done-list" element={<DoneList/>} />
               <Route path="/todo-list" element={<TodoList />} />
-           </Routes>
-        </Router>  
+          </Routes>
+      </Router>  
+
       </TodoContext.Provider>
+
     </div>
   );
 }
