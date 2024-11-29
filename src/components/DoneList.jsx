7fb96b5c4bcd14801aece37c8357be5bb752e905
Emily Ng <ngem3@oocl.com>
2024-11-29 11:20:38 +0800

    feat: add doneList

---
+++ src/components/DoneList.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react"
+import { TodoContext } from "../App"
+
+export default function DoneList(){
+    const { state } = useContext(TodoContext);
+
+    const doneList = (state) => {
+        console.log(state)
+        return (state.filter(todo => todo.done).map((todo) => 
+         (<p key={todo.id}>{todo.text}</p>)))        
+    }
+
+    return (<>
+        <h1>Done List</h1>
+        {doneList(state)}
+    </>)
+}
