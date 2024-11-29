ca5e2ac23fdbed5db3d0f095dca21cf4fad99c93
Emily Ng <ngem3@oocl.com>
2024-11-29 15:21:06 +0800

    feat: use getTodos api to fetch todo list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,24 @@
+import TodoGenerator from "./TodoGenerator";
+import TodoGroup from "./TodoGroup";
+import React from "react";
+import { getTodos } from "../api/todo";
+import { TodoContext } from "../App";
+import { Actions } from "../context/todoReducer";
 
-import TodoGenerator from './TodoGenerator';
-import TodoGroup from './TodoGroup';
+export default function TodoList() {
+  const { dispatch } = React.useContext(TodoContext);
 
-export default function TodoList () {
+  React.useEffect(() => {
+    getTodos().then((todos) => {
+      dispatch({ type: Actions.INIT, payload: todos });
+    });
+  }, []);
+
   return (
-      <div>
-        <h1>This is the TodoList Component</h1>
-        <TodoGroup/>
-        <TodoGenerator/>
-      </div>
+    <div>
+      <h1>This is the TodoList Component</h1>
+      <TodoGroup />
+      <TodoGenerator />
+    </div>
   );
 }
--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -1,19 +1,30 @@
+export const InitialState = [];
+
+export const Actions = {
+  ADD: "ADD",
+  UPDATE_DONE: "UPDATE_DONE",
+  DELETE: "DELETE",
+  INIT: "INIT",
+};
+
 export const todoReducer = (state, action) => {
   switch (action.type) {
-    case "ADD":
+    case Actions.ADD:
       return [
         ...state,
         { id: Date.now(), text: action.payload.text, done: false },
       ];
-    case "UPDATE_DONE":
+    case Actions.UPDATE_DONE:
       return state.map((todo) => {
         if (todo.id === action.payload.id) {
           return { ...todo, done: !todo.done };
         }
         return todo;
       });
-    case "DELETE":
+    case Actions.DELETE:
       return state.filter((todo) => todo.id !== action.payload.id);
+    case Actions.INIT:
+      return action.payload;
     default:
       return state;
   }
