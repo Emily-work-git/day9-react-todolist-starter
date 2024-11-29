eb1650e95a923aa391adafd8f863b21a700f9702
Emily Ng <ngem3@oocl.com>
2024-11-29 16:04:21 +0800

    feat: add addTodos api

--- src/api/todo.js
+++ src/api/todo.js
@@ -2,11 +2,20 @@ import axios from 'axios';
 
 const baseURL = 'https://67495c7e868020296630aada.mockapi.io/api/v1';
 
-const instance = axios.create({baseURL, timeout: 1000});
+const instance = axios.create({baseURL});
 
 export const getTodos = async () => {
     try{
-        const response = await instance.get("/TodoItem");
+        const response = await instance.get("/todo");
+        return response.data;
+    }catch(error){
+        console.error(error);
+    }
+}
+
+export const addTodos = async (data) => {
+    try{
+        const response = await instance.post("/todo",data);
         return response.data;
     }catch(error){
         console.error(error);
