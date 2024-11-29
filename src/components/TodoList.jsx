9647a4020cba7a55dba677e044bbddb79e1a0521
Emily Ng <ngem3@oocl.com>
2024-11-29 16:22:32 +0800

    feat: display antDesign spin when page is loading

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,17 +4,25 @@ import React from "react";
 import { getTodos } from "../api/todo";
 import { TodoContext } from "../App";
 import { Actions } from "../context/todoReducer";
+import { Spin } from "antd";
 
 export default function TodoList() {
   const { dispatch } = React.useContext(TodoContext);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-    getTodos().then((todos) => {
-      dispatch({ type: Actions.INIT, payload: todos });
-    });
+    getTodos()
+      .then((todos) => {
+        dispatch({ type: Actions.INIT, payload: todos });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  return (
+  return isLoading ? (
+    <Spin></Spin>
+  ) : (
     <div>
       <h1>This is the TodoList Component</h1>
       <TodoGroup />
